35ac4acf933c1e66e4a11e7036d0d914f14683be
adam-weiler <adam.weiler@yahoo.ca>
2019-08-02 17:14:50 -0400

    Adding minlength, maxlength validators on Product models, making migrations and migrating changes.

--- rainforest/models.py
+++ rainforest/models.py
@@ -1,15 +1,14 @@
 from django.db import models
 # from django import forms
-# from django.core.validators import MinValueValidator
 
-from django.core.validators import MinValueValidator
+from django.core.validators import MinLengthValidator, MaxLengthValidator, MinValueValidator
 # from django.core.exceptions import ValidationError
 # from django.contrib.auth.models import User
 
 
 class Product(models.Model):
     name = models.CharField(max_length=300)
-    description = models.TextField(blank=True, null=True)
+    description = models.TextField(validators=[MinLengthValidator(10), MaxLengthValidator(500)])
     price_in_cents = models.IntegerField(validators=[MinValueValidator(1)], null=True)
 
     def __str__(self):
