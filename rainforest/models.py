60ce0a3c4671b00041cf235b5580aaefae64b3dd
adam-weiler <adam.weiler@yahoo.ca>
2019-08-02 13:57:34 -0400

    Created the Product model, make migrations, migrated changes to database.

---
+++ rainforest/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+# from django import forms
+# from django.core.validators import MinValueValidator
+
+from django.core.validators import MinValueValidator
+# from django.core.exceptions import ValidationError
+# from django.contrib.auth.models import User
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=300)
+    description = models.TextField(blank=True, null=True)
+    price_in_cents = models.IntegerField(validators=[MinValueValidator(1)], null=True)
+
+    def __str__(self):
+        return f"name = {self.name}"
